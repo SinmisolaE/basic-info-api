56c59059da8c3cc033fe2f776493feafa5cc5d06
SinmisolaE <sinmisolaajao@gmail.com>
2025-01-30 18:36:28 +0000

    app commented

--- app.py
+++ app.py
@@ -6,26 +6,27 @@ from datetime import datetime
 import os
 
 
-app = Flask(__name__)
+app = Flask(__name__)  #creates flask instance
 
 
-
+# get environment variables
 email = os.getenv('EMAIL')
 github_url = os.getenv('GITHUB_URL')
 
+# Handle Cross Origin Resource Sharing for security
 CORS(app, resources={r"/": {"origins": https://basic-info-api-kgli.onrender.com/}})
-print(email)
 
-
-@app.route("/", methods=['GET'])
+@app.route("/", methods=['GET'])   # app endpoint
 def basic_info():
     """ method returns email, current datetime and github url"""
-    current_datetime = datetime.now(datetime.timezone.utc).isoformat().replace("+00:00", "Z")
+    current_datetime = datetime.now(datetime.timezone.utc)   # datetime in iso format utc
+                            .isoformat().replace("+00:00", "Z")
     data = {
         "email": email,
         "current time": current_datetime,
         "github url": github_url
     }
-    return jsonify(data), 200
+    return jsonify(data), 200   
+
 if __name__ == '__main__':
     app.run(debug=True)

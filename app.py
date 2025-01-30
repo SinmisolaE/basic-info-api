f355d5538544d9ac74f8e4af176502d503569431
SinmisolaE <sinmisolaajao@gmail.com>
2025-01-30 18:19:29 +0000

    app updated

--- app.py
+++ app.py
@@ -4,23 +4,23 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from datetime import datetime
 import os
-from dotenv import load_dotenv
+
 
 app = Flask(__name__)
 
-load_dotenv()
+
 
 email = os.getenv('EMAIL')
 github_url = os.getenv('GITHUB_URL')
 
-#CORS(app, resources={r"/api": {"origins": http://127.0.0.1:5000/}})
+CORS(app, resources={r"/": {"origins": https://basic-info-api-kgli.onrender.com/}})
 print(email)
 
 
 @app.route("/", methods=['GET'])
 def basic_info():
     """ method returns email, current datetime and github url"""
-    current_datetime = datetime.utcnow().isoformat() + 'Z'
+    current_datetime = datetime.now(datetime.timezone.utc).isoformat().replace("+00:00", "Z")
     data = {
         "email": email,
         "current time": current_datetime,
